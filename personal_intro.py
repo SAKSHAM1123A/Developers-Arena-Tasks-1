dc4b96f00e352ae8241197fe194420e6c11e4d94
Saksham Bhatnagar <sakshambhatnagar185@gmail.com>
2026-06-11 05:15:53 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,17 @@
+
+Name = input("What is your name? :- ")
+Age = int(input("What is your age? :- "))
+Hobby = input("What is your hobby? :-")
+City = input("In which city do you live? :- ")
+State = input("In what state do you live? :-")
+Occupation = input("What is your occupation? :- ")
+
+
+print(f"🎉 Welcome {Name}! 🎉")
+print(f"Age :- Your {Age} Years old")
+print(f"Hobby :- Your hobby is {Hobby}")
+print(f"City :- YOu live in {City} city")
+print(f"State :- You live in {State}")
+print(f"Occupation :- You are currently a {Occupation}")
+
+print(f"We welcome {Name} who is {Age} years old and is currently a {Occupation} ")
